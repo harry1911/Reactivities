1a02d3db2dae87393913c4f51fd46d202f627916
Hector Masson <h.masson1911@gmail.com>
2020-12-02 12:30:14 -0500

    Adding the project to source control

---
+++ Persistence/Migrations/20200728235035_FixingSeedValues.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class FixingSeedValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "Value 001");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Value 002");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "Value 003");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "001");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "010");
+
+            migrationBuilder.UpdateData(
+                table: "Values",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Name",
+                value: "011");
+        }
+    }
+}
---
+++ Reactivities/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reactivities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
